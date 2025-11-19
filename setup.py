1967d856ef24fa2f766903c55e19f312274299ac
Emin Fedar <emin.fedar@pardus.org.tr>
2025-11-19 16:44:46 +0300

    feat: submit window added, refactored managers, ACPI support

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# 
-# This file is template/test 
-# 
+#
+# This file is template/test
+#
 
 import os
 import subprocess
@@ -15,12 +15,20 @@ def create_mo_files():
     mo = []
     for po in os.listdir(podir):
         if po.endswith(".po"):
-            os.makedirs("{}/{}/LC_MESSAGES".format(podir, po.split(".po")[0]), exist_ok=True)
-            mo_file = "{}/{}/LC_MESSAGES/{}".format(podir, po.split(".po")[0], "pardus-python-gtk.mo")
-            msgfmt_cmd = 'msgfmt {} -o {}'.format(podir + "/" + po, mo_file)
+            os.makedirs(
+                "{}/{}/LC_MESSAGES".format(podir, po.split(".po")[0]), exist_ok=True
+            )
+            mo_file = "{}/{}/LC_MESSAGES/{}".format(
+                podir, po.split(".po")[0], "pardus-about.mo"
+            )
+            msgfmt_cmd = "msgfmt {} -o {}".format(podir + "/" + po, mo_file)
             subprocess.call(msgfmt_cmd, shell=True)
-            mo.append(("/usr/share/locale/" + po.split(".po")[0] + "/LC_MESSAGES",
-                       ["po/" + po.split(".po")[0] + "/LC_MESSAGES/pardus-python-gtk.mo"]))
+            mo.append(
+                (
+                    "/usr/share/locale/" + po.split(".po")[0] + "/LC_MESSAGES",
+                    ["po/" + po.split(".po")[0] + "/LC_MESSAGES/pardus-about.mo"],
+                )
+            )
     return mo
 
 
@@ -37,39 +45,50 @@ if os.path.exists(changelog):
     f.close()
 
 data_files = [
-    ("/usr/bin", ["pardus-python-gtk"]),
-    ("/usr/share/applications",
-     ["data/tr.org.pardus.python-gtk.desktop"]),
-    ("/usr/share/pardus/pardus-python-gtk/ui",
-     ["ui/MainWindow.glade"]),
-    ("/usr/share/pardus/pardus-python-gtk/src",
-     ["src/Main.py",
-      "src/MainWindow.py",
-      "src/UserSettings.py",
-      "src/__version__"]),
-    ("/usr/share/pardus/pardus-python-gtk/data",
-     ["data/style.css",
-      "data/tr.org.pardus.python-gtk-autostart.desktop",
-      "data/pardus-python-gtk.svg",
-      "data/pardus-python-gtk-on-symbolic.svg",
-      "data/pardus-python-gtk-off-symbolic.svg"]),
-    ("/usr/share/icons/hicolor/scalable/apps/",
-     ["data/pardus-python-gtk.svg",
-      "data/pardus-python-gtk-on-symbolic.svg",
-      "data/pardus-python-gtk-off-symbolic.svg"])
+    ("/usr/bin", ["pardus-about"]),
+    ("/usr/share/applications", ["data/tr.org.pardus.python-gtk.desktop"]),
+    ("/usr/share/pardus/pardus-about/ui", ["ui/MainWindow.glade"]),
+    (
+        "/usr/share/pardus/pardus-about/src",
+        [
+            "src/Main.py",
+            "src/MainWindow.py",
+            "src/UserSettings.py",
+            "src/Actions.py",
+            "src/__version__",
+        ],
+    ),
+    (
+        "/usr/share/pardus/pardus-about/data",
+        [
+            "data/style.css",
+            "data/tr.org.pardus.python-gtk-autostart.desktop",
+            "data/pardus-about.svg",
+            "data/pardus-about-on-symbolic.svg",
+            "data/pardus-about-off-symbolic.svg",
+        ],
+    ),
+    (
+        "/usr/share/icons/hicolor/scalable/apps/",
+        [
+            "data/pardus-about.svg",
+            "data/pardus-about-on-symbolic.svg",
+            "data/pardus-about-off-symbolic.svg",
+        ],
+    ),
 ] + create_mo_files()
 
 setup(
-    name="pardus-python-gtk",
+    name="pardus-about",
     version=version,
     packages=find_packages(),
-    scripts=["pardus-python-gtk"],
+    scripts=["pardus-about"],
     install_requires=["PyGObject"],
     data_files=data_files,
     author="Yusuf Düzgün",
     author_email="yusuf.duzgun@pardus.org.tr",
     description="Pardus Python GTK EXAMPLE",
     license="GPLv3",
-    keywords="pardus-python-gtk, example, test",
-    url="https://github.com/pardus/pardus-python-gtk",
+    keywords="pardus-about, example, test",
+    url="https://github.com/pardus/pardus-about",
 )
